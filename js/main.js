89c96acc002a8ba1ace31bd9618628bebdc37f44
Miguel Augusto Aliaga Pantoja <maliaga.pantoja@gmail.com>
2016-03-09 15:21:58 -0500

    initial

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/**
+ * Created by dev on 9/03/16.
+ */
+//show validate code form
+document.querySelector('.box.main button')
+        .addEventListener('click',function(){
+           document.querySelector('.box.validate-code')
+               .setAttribute('data-activate','');
+        });
+//validating code
+document.querySelector('.validate-code button')
+        .addEventListener('click',function(e){
+            e.preventDefault();
+            document.querySelector('.box.welcome')
+                .setAttribute('data-activate','')
+        });
+document.querySelector('.welcome button')
+    .addEventListener('click',function(e){
+        e.preventDefault();
+        document.querySelector('.box.user-form')
+            .setAttribute('data-left','')
+    });
