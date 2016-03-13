fd1a111eae5e609cdddd671377de199118163f69
Miguel Augusto Aliaga Pantoja <maliaga.pantoja@gmail.com>
2016-03-13 14:49:39 -0500

    icono de cerrar

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
 document.querySelector('.box.main button')
         .addEventListener('click',function(){
            document.querySelector('.box.validate-code')
-               .setAttribute('data-activate','');
+               .setAttribute('data-activate-window','');
         });
 //validating code
 document.querySelector('.validate-code button')
@@ -19,4 +19,8 @@ document.querySelector('.welcome button')
         e.preventDefault();
         document.querySelector('.box.user-form')
             .setAttribute('data-left','')
+    });
+document.querySelector('.close')
+    .addEventListener('click',function(){
+       this.parentNode.removeAttribute('data-activate-window');
     });
